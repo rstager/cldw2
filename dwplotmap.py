ab16c44f13e9d73489f1ec44b2345b33ca4b7895
Roger Stager <roger@stager.us>
2017-05-03 12:57:54 -0700

    Added tprop to display

--- dwplotmap.py
+++ dwplotmap.py
@@ -12,6 +12,7 @@ class Tag:
         self.x=[]
         self.y=[]
         self.t=[]
+        self.tprop=0
 
 logging = True
 
@@ -31,13 +32,20 @@ fig.canvas.mpl_connect('pick_event', onpick3)
 def on_message(client, userdata, msg):
     report = json.loads(msg.payload)
     report=bunchify(report)
+
     if report.type=='tag':
         series[report.id].x.append(report.x)
         series[report.id].y.append(report.y)
         series[report.id].t.append(report.t)
+        if not 'tprops' in report: return
+        for id, v in aseries.items():
+            v.tprop=0
+        for (id,t) in report.tprops:
+            aseries[id].tprop=t
     else:
-        aseries[report.id].x.append(report.x)
-        aseries[report.id].y.append(report.y)
+        aseries[report.id].x=report.x
+        aseries[report.id].y=report.y
+        return
 
     plt.clf()
     plt.axis([0,40,0,28])
@@ -45,7 +53,14 @@ def on_message(client, userdata, msg):
         plt.plot(v.x[-100:],v.y[-100:])
         plt.plot(v.x[-1:],v.y[-1:],marker='o',markersize=10,label=id,gid=v.t)
     for id,v in aseries.items():
-        plt.plot(v.x,v.y,marker='+',markersize=10,label=id)
+        plt.plot(v.x,v.y,marker='+',markersize=30,label=id)
+        #plt.scatter(v.x, v.y, marker='o', s=(v.tprop)*100, facecolors='none', edgecolors='r')
+    ax = plt.gca()
+    for id, v in aseries.items():
+        if v.tprop == 0 : continue
+        print(id,v.tprop,v.x,v.y)
+        circle1 = plt.Circle((v.x,v.y), v.tprop*0.9835, color='r',fc='none',alpha=0.2)
+        ax.add_artist(circle1)
     plt.legend(loc='lower left', shadow=True)
     plt.pause(0.1)
 
